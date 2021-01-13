ca6b74c077bc7051ae871a43281548a26c2ef35b
Sanket Patil <sanket.9006@gmail.com>
2021-01-13 20:39:34 +0530

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Image from 'react-bootstrap/Image';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Image src="https://react-coming-soon.maksv.me/web-development.svg" 
+        className="App-logo"  
+        alt="comingsoon" 
+        fluid="true" 
+        height="600px" 
+        width="600px">
+        </Image>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming Soon
         </p>
         <a
           className="App-link"
