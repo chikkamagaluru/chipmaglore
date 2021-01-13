73d83ff03695023858a08ae32df54c60e8eac566
Sanket Patil <sanket.9006@gmail.com>
2021-01-13 22:22:30 +0530

    Email Address Functionality - Subscribe

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import Comingsoon from './components/comingsoon/comingsoon.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://react-coming-soon.maksv.me/web-development.svg" className="App-logo" alt="logo" />
-        <p>
-         Coming Soon
-        </p>
-        <a
-          className="App-link"
-          href="https://chipmaglore.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          www.chipmaglore.org
-        </a>
-      </header> 
       <Comingsoon />
     </div>
   );
--- src/components/comingsoon/comingsoon.js
+++ src/components/comingsoon/comingsoon.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './comingsoon.css';
+import {Form, Col} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function comingsoon(props) {
     return (
@@ -16,6 +21,19 @@ function comingsoon(props) {
             rel="noopener noreferrer">
             www.chipmaglore.org
           </a>
+
+          <Form>
+          <Form.Row>  
+            <Col>
+              <Form.Control placeholder="Email Address" /> </Col>
+            <Col xs="auto">
+              <Button type="submit" className="mb-2">
+                Submit
+              </Button>
+            </Col>
+          </Form.Row>
+        </Form>
+
         </header> 
       </div>
     );
