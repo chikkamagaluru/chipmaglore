d1d9afc69560b36218aad9c1d172418d12cce3f5
Sanket Patil <sanket.9006@gmail.com>
2021-01-13 20:51:32 +0530

    Coming Soon

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Image from 'react-bootstrap/Image';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Image src="https://react-coming-soon.maksv.me/web-development.svg" 
-        className="App-logo"  
-        alt="comingsoon" 
-        fluid="true" 
-        height="600px" 
-        width="600px">
-        </Image>
-
+        <img src="https://react-coming-soon.maksv.me/web-development.svg" className="App-logo" alt="logo" />
         <p>
-          Coming Soon
+         Coming Soon
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://chipmaglore.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+          www.chipmaglore.org
         </a>
-      </header>
+      </header> 
     </div>
   );
 }
