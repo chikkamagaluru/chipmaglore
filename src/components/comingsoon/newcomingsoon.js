cd4920938b42b3af600a5947df642d4ea8474bdc
Sanket Patil <sanket.9006@gmail.com>
2021-01-14 21:39:50 +0530

    Removed other social media icons

--- src/components/comingsoon/newcomingsoon.js
+++ src/components/comingsoon/newcomingsoon.js
@@ -5,15 +5,10 @@ import { Form, Col, Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import desktopImage from './images/chikkamagaluru-desktop1.jpg';
+import desktopImage from './images/chikkamagaluru-desktop.jpg';
 import mobileImage from './images/chikkamagaluru-mobile.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faYoutube,
-  faFacebook,
-  faTwitter,
-  faInstagram
-} from "@fortawesome/free-brands-svg-icons";
+import {faInstagram} from "@fortawesome/free-brands-svg-icons";
 
 const App = () => {
   const imageUrl = window.innerWidth >= 1081 ? desktopImage : mobileImage;
@@ -25,19 +20,7 @@ const App = () => {
         <Container>
           <div class="social-container">
             <Row className="justify-content-center">
-
-            <a href="https://www.youtube.com/channel/UC_MSvFCcvGWIZKZG2XgGSuA"
-              className="youtube social">
-              <FontAwesomeIcon icon={faYoutube} size="2x" />
-            </a>
-            <a href="https://www.facebook.com/profile.php?id=100039368761634"
-              className="facebook social">
-              <FontAwesomeIcon icon={faFacebook} size="2x" />
-            </a>
-            <a href="https://twitter.com/sanket__patil" className="twitter social">
-              <FontAwesomeIcon icon={faTwitter} size="2x" />
-            </a>
-            <a href="https://www.instagram.com/____sanket____patil____/"
+            <a href="https://instagram.com/chikkamagaluru?igshid=kj1s78b2ydlz"
               className="instagram social">
               <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
